ea199e053b762ef535cd55e89b2caf693390a441
Unknown <jcmansillam@correo.url.edu.gt>
2017-11-17 11:09:46 -0600

    Last change

--- routes/profile.js
+++ routes/profile.js
@@ -90,7 +90,7 @@ function comp (){
     console.log("  DLL>>>>")
     console.log("  File name => "+path);
     CompressWithDll( "./uploads/" + path, function (error, result) {
-        if(error) throw console.log(error);
+        if(error) console.log(error);
         console.log("  compress result: ");
         console.log(result);
     });
@@ -100,7 +100,7 @@ function desc(){
     console.log("  DLL>>>>")
     console.log("  File name => "+ "./uploads/" + path +".rlex");
     DecompressWithDll( "./uploads/" + path +".rlex", function (error, result) {
-        if(error) throw error;
+        if(error) console.log(error);
         console.log("  decompress result: ");
         console.log(result);
     });
